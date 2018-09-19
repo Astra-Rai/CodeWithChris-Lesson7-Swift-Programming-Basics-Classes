3943b9c967e99633ea0792d55a12494c265e8158
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-09-18 23:50:27 -0700

    Add files via upload

---
+++ CodeWithChris-Lesson7-Classes.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Spaceship {
+
+var fuelLevel = 100
+
+    func cruise(){
+    //code to initiate cruising
+    print(fuelLevel)
+    }
+    func thrust(){
+    //code inititates rocket thrus
+    print(fuelLevel)
+    }
+}
+//methods are functions that are associated with a particular type
+//instance methods are functions that belong to instance of a particular type
+//fuelLevel, is a proper of the class Spaceship
+//functions cruise and thrust
+
